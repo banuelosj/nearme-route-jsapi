baaa37ece1086e38ade76ea1c58bcffdce9539eb
Jose Banuelos <JBanuelos@esri.com>
2020-05-03 10:24:37 -0700

    display symbols from query

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ require([
   "esri/views/MapView",
   "esri/Map",
   "esri/layers/FeatureLayer",
-], function (MapView, Map, FeatureLayer) {
+  "esri/layers/GraphicsLayer",
+  "esri/Graphic",
+], function (MapView, Map, FeatureLayer, GraphicsLayer, Graphic) {
+  const radius = 5;
+  const distanceUnits = "miles";
+  const graphicsLayer = new GraphicsLayer();
+
   const facilitiesLayer = new FeatureLayer({
     url:
       "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0",
@@ -11,7 +17,7 @@ require([
 
   const map = new Map({
     basemap: "streets",
-    layers: [facilitiesLayer],
+    layers: [graphicsLayer],
   });
 
   const view = new MapView({
@@ -29,9 +35,46 @@ require([
         const graphic = response.results.filter((result) => {
           return result.graphic.layer === facilitiesLayer;
         })[0].graphic;
-
         console.log(graphic.attributes);
+      } else {
+        const mapPoint = view.toMap(response.screenPoint);
+        findFacilities(mapPoint, facilitiesLayer);
       }
+    });
+  }
+
+  // query nearby facilities within a certain radious
+  function findFacilities(point, layer) {
+    const query = layer.createQuery();
+    query.returnGeometry = true;
+    query.distance = radius;
+    query.units = distanceUnits;
+    query.outFields = ["*"];
+    query.geometry = point;
+
+    layer.queryFeatures(query).then((results) => {
+      displayLocations(results.features);
+    });
+  }
+
+  function displayLocations(features) {
+    //clear existing graphics first
+    graphicsLayer.removeAll();
+
+    const locationSymbol = {
+      type: "simple-marker",
+      path:
+        "M15.999 0C11.214 0 8 1.805 8 6.5v17l7.999 8.5L24 23.5v-17C24 1.805 20.786 0 15.999 0zM16 14.402A4.4 4.4 0 0 1 11.601 10a4.4 4.4 0 1 1 8.798 0A4.4 4.4 0 0 1 16 14.402z",
+      color: "#0079C1",
+      size: "15px",
+    };
+
+    features.forEach((feature) => {
+      const graphic = new Graphic({
+        geometry: feature.geometry,
+        symbol: locationSymbol,
+      });
+      graphicsLayer.add(graphic);
     });
   }
 });
