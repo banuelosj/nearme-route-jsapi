47db5356852bbcdf891793dea79af352028705ee
Jose Banuelos <JBanuelos@esri.com>
2020-05-03 11:29:55 -0700

    buffer graphic added

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ require([
   "esri/layers/FeatureLayer",
   "esri/layers/GraphicsLayer",
   "esri/Graphic",
-], function (MapView, Map, FeatureLayer, GraphicsLayer, Graphic) {
+  "esri/geometry/geometryEngine",
+], function (
+  MapView,
+  Map,
+  FeatureLayer,
+  GraphicsLayer,
+  Graphic,
+  geometryEngine
+) {
   const radius = 5;
   const distanceUnits = "miles";
   const graphicsLayer = new GraphicsLayer();
@@ -35,25 +43,72 @@ require([
         const graphic = response.results.filter((result) => {
           return result.graphic.layer === facilitiesLayer;
         })[0].graphic;
-        console.log(graphic.attributes);
       } else {
         const mapPoint = view.toMap(response.screenPoint);
-        findFacilities(mapPoint, facilitiesLayer);
+        addPointToMap(mapPoint);
+        // create the buffer to display and for the query
+        const buffer = addBuffer(mapPoint);
+        findFacilities(buffer, facilitiesLayer);
       }
     });
   }
 
+  function addPointToMap(point) {
+    //clear existing graphics
+    if (view.graphics.length > 0) {
+      view.graphics.removeAll();
+    }
+
+    const locationSymbol = {
+      type: "simple-marker",
+      path:
+        "M15.999 0C11.214 0 8 1.805 8 6.5v17l7.999 8.5L24 23.5v-17C24 1.805 20.786 0 15.999 0z",
+      color: "#de2900",
+      size: "35px",
+    };
+
+    const pointGraphic = new Graphic({
+      geometry: point,
+      symbol: locationSymbol,
+    });
+
+    view.graphics.add(pointGraphic);
+  }
+
+  function addBuffer(point) {
+    const polySym = {
+      type: "simple-fill", // autocasts as new SimpleFillSymbol()
+      color: [140, 140, 222, 0.5],
+      outline: {
+        color: [0, 0, 0, 0.5],
+        width: 2,
+        style: "dash",
+      },
+    };
+
+    const buffer = geometryEngine.geodesicBuffer(point, radius, distanceUnits);
+
+    const bufferGraphic = new Graphic({
+      geometry: buffer,
+      symbol: polySym,
+    });
+
+    view.graphics.add(bufferGraphic);
+    return buffer;
+  }
+
   // query nearby facilities within a certain radious
-  function findFacilities(point, layer) {
+  function findFacilities(buffer, layer) {
     const query = layer.createQuery();
     query.returnGeometry = true;
-    query.distance = radius;
-    query.units = distanceUnits;
+    //  query.distance = radius;
+    //  query.units = distanceUnits;
     query.outFields = ["*"];
-    query.geometry = point;
+    query.geometry = buffer;
 
     layer.queryFeatures(query).then((results) => {
       displayLocations(results.features);
+      //populateCards();
     });
   }
 
@@ -61,7 +116,7 @@ require([
     //clear existing graphics first
     graphicsLayer.removeAll();
 
-    const locationSymbol = {
+    const facilitySymbol = {
       type: "simple-marker",
       path:
         "M15.999 0C11.214 0 8 1.805 8 6.5v17l7.999 8.5L24 23.5v-17C24 1.805 20.786 0 15.999 0zM16 14.402A4.4 4.4 0 0 1 11.601 10a4.4 4.4 0 1 1 8.798 0A4.4 4.4 0 0 1 16 14.402z",
@@ -72,7 +127,7 @@ require([
     features.forEach((feature) => {
       const graphic = new Graphic({
         geometry: feature.geometry,
-        symbol: locationSymbol,
+        symbol: facilitySymbol,
       });
       graphicsLayer.add(graphic);
     });
